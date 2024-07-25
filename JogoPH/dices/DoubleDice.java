6d80c2bad10a0f4ddf696173f2010638acd1cacd
mariminha <147452796+mariminha@users.noreply.github.com>
2024-07-25 08:17:29 -0300

    Update DoubleDice.java

--- JogoPH/dices/DoubleDice.java
+++ JogoPH/dices/DoubleDice.java
@@ -1,45 +1,46 @@
 package dices;
 import java.util.Random; //bibliotéca de números aleatórios
+import visual.*;
 
 public class DoubleDice {
     protected Random rdm; //objetico que cria números aleatórios
     protected int dice1;
     protected int dice2;
+
     /*Iniciando Dado Duplo*/
     public DoubleDice(){
         this.rdm = new Random();
         this.dice1 = 0;
         this.dice2 = 0;
     }
-    
-    protected int ThrowDice(int dice) {
-    	if(dice == 1) {
-    		this.dice1 = rdm.nextInt(6) + 1;
-    		return this.dice1;
-    	}
-    	else if(dice == 2) {
-    		this.dice2 = rdm.nextInt(6) + 1;
-    		return this.dice2;
-    	}
-    	else {
-    		return 0;
-    	}
+
+    public int ThrowDice(int dice) {
+        if(dice == 1) {
+            dice1 = rdm.nextInt(6) + 1;
+            return dice1;
+        }
+        else if(dice == 2) {
+            dice2 = rdm.nextInt(6) + 1;
+            return dice2;
+        }
+        else {
+            return 0;
+        }
     }
-    
+
     /*Jogando os dados*/
     public int ThrowDices(){
-        return  ThrowDice(1) + ThrowDice(2) ; //Dados Normais, vão de 2 a 12
+        int sum = ThrowDice(1) + ThrowDice(2); //Dados Normais, vão de 2 a 12
+        Visual.addDados(dice1, dice2);
+        return sum;
     }
-    
-    
-    public boolean DicesAreEqual() {
-    	if(this.dice1 == this.dice2 && this.dice1 != 0) {
-    		return true;
-    	}
-    	else {
-    		return false;
-    	}
-    }
-    
 
+    public boolean DicesAreEqual() {
+        if(dice1 == dice2 && dice1 != 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
