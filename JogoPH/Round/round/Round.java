078593976f7e9aa7c0bf3a3b08e08f869fb39508
mariminha <147452796+mariminha@users.noreply.github.com>
2024-07-25 08:16:06 -0300

    Update Round.java

--- JogoPH/Round/round/Round.java
+++ JogoPH/Round/round/Round.java
@@ -1,124 +1,93 @@
 package round;
 import entities.*;
-import java.util.Scanner;
+import visual.*;
 
 public class Round {
-	/*constants - indica as casas especiais*/
-	private int[] dont_play_houses = {10,25,38};
-	private int[] surprise_house = {13};
-	private int[] luck_houses = {5,15,30};
-	private int[] choose_to_init_houses = {17,27};
-	private int[] magic_houses = {20,35};
-	
-	/*attributes*/
-	private Persons player;//onde os jogadores ficarão armazenados
-	
-	/*constructor*/
-	public Round(Persons person) {
-		this.player = person;	
-	}
+    
+    /*constants - indica as casas especiais*/
+    private int[] dont_play_houses = {9,24,37};
+    private int[] surprise_house = {12};
+    private int[] luck_houses = {4,14,29};
+    private int[] choose_to_init_houses = {16,26};
+    private int[] magic_houses = {19,34};
 
-	public Round() {};
-	
-	/*gets and sets*/
-	public Persons getPerson() {
-		return player;
-	}
-	public void setPerson(Persons person) {
-		this.player = person;
-	}
-	
-	/*methods*/
-	
-	private boolean IsIn(int number, int[] arr) {
-		for(int element : arr) {
-			if(number == element) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	
-	public void makeRound(Board b) {//Eu fiz umas pequenas alterações para as peças andarem
-		if(player.isDont_play() == false) {
-	        int y = 0;
-			player.rollDice();//anda
-        	b.removePerson(player.getColor(), player.getHouse());  
-			player.walk();
-	        if(player.getHouse() >= 39){
-	            int x = player.getHouse() - 39;
-	            b.addPerson(player.getColor(), 39);
-	            b.printBoard();
-	            System.out.println("+" + x);
-	            y++;
-	        }
-			int house = player.getHouse();
-	        if(y == 0){
-	            b.addPerson(player.getColor(), house);
-	        }
-			player.setDont_play(IsIn(house, this.dont_play_houses));
-			player.setSurprise(IsIn(house, this.surprise_house)); 
-			player.setLuck(IsIn(house, this.luck_houses));
-			player.setMagic(IsIn(house, this.magic_houses));
-			player.setChooseToInit(IsIn(house, this.choose_to_init_houses));
-		}
-		else {
-			player.setDont_play(false);
-			System.out.println("passou a vez");
-		}
-	}
-	
-	
-	public void applyStatusEffects(Persons[] persons) {
-		if(player.isDont_play() == true) {
-			
-		}
-		else if(player.isChooseToInit() == true) {
-			Scanner scn = new Scanner(System.in);
-			String[] colors = new String[6];
-			for(int i = 0; i < persons.length;i++) {
-				colors[i] = persons[i].getColor();
-			}
-			
-			String options = new String();
-			for(int i = 0; i < colors.length; i++) {
-				options.concat(Integer.toString(i) + " : " + colors[i] + ";");
-			}
-			int choice = 0;
-			while(choice >= colors.length || choice <= 0) {
-				System.out.println("Choose a player to go to de 0st house: " + options);
-				choice = scn.nextInt();
-			}
-			
-			persons[choice].setHouse(0);
-			scn.close();
-			
-		}
-		else if(player.isMagic() == true) {
-			
-			/*achando o menor*/
-			int smaller_value = 42;
-			int lower_index = 0;
-			for (int i = 0; i < persons.length; i++) {
-				if (persons[i].getHouse() < smaller_value) {
-	              smaller_value = persons[i].getHouse();
-	                lower_index = i; // Atualiza a posição do menor valor encontrado
-	            }
-	        }
-			
-			player.setHouse(persons[lower_index].getHouse());//take the place of the last one
-			persons[lower_index].setHouse(player.getHouse());
-			
-		}
-		else if(player.isLuck() == true) {
-			player.setHouse(player.getHouse() + 3);//walk 3 more houses
-		}
-		else if(player.isSurprise() == true) {
-			player.setTypeToRandom();
-		}
-	}
-	
+    /*attributes*/
+    private Persons player;//onde os jogadores ficarão armazenados
 
-	
+    /*constructor*/
+    public Round(Persons person) {
+        this.player = person;
+    }
+    // public Round() {};
+
+    /*gets and sets*/
+    public Persons getPerson() {
+        return player;
+    }
+    public void setPerson(Persons person) {
+        this.player = person;
+    }
+
+    /*methods*/
+
+    private boolean IsIn(int number, int[] arr) {
+        for(int element : arr) {
+            if(number == element) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void makeRound() {//onde o jogador fará sua jogada.
+        player.rollDice();//joga dado
+        player.walk();//anda
+        int house = player.getHouse();
+        player.setDont_play(IsIn(house, this.dont_play_houses));
+        player.setSurprise(IsIn(house, this.surprise_house));
+        player.setLuck(IsIn(house, this.luck_houses));
+        player.setMagic(IsIn(house, this.magic_houses));
+        player.setChooseToInit(IsIn(house, this.choose_to_init_houses));
+    }
+
+    public void applyStatusEffects(Persons[] persons) {
+        if(player.isChooseToInit()) {
+            int choice = Visual.voltarUmAoInicio();
+            persons[choice].setHouse(0);
+            player.setChooseToInit(false);
+        }
+        else if(player.isMagic()) {
+            /*achando o menor*/
+            int smaller_value = 42;
+            int pessoaAtras = 0;
+            for (int i = 0; i < Visual.getValor(); i++) {
+                if (persons[i].getHouse() < smaller_value) {
+                    smaller_value = persons[i].getHouse();
+                    pessoaAtras = i; // Atualiza a posição do menor valor encontrado
+                }
+            }
+            if(pessoaAtras != player.getHouse()) {
+                int atras = persons[pessoaAtras].getHouse();
+                int frente = player.getHouse();
+                Visual.trocar(player.getColor(), persons[pessoaAtras].getColor(), frente, atras);
+                persons[pessoaAtras].setHouse(frente);
+                player.setHouse(atras);
+            }
+            player.setMagic(false);
+        }
+        else if(player.isLuck()) {
+            if(player.getType() != 'm') {
+                player.setHouse(player.getHouse() + 3);//walk 3 more houses
+                Visual.andar(player.getColor(), player.getHouse(), 3);
+                player.setLuck(false);
+            }
+            else{
+                player.setLuck(false);
+            }
+        }
+        else if(player.isSurprise()) {
+            player.setTypeToRandom();
+            player.setSurprise(false);
+        }
+    }
 }
